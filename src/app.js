7dbcd42e5aba03a3537a87721e88b6dba36e78ad
khashashin <khashashin@gmail.com>
2017-07-28 00:17:41 +0200

    added some API functionality / full content loaded bei using API(from json file)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import Post from "./post";
+import User from "./user";
 import ui from "./ui.js";
 
 Post.findAll()
   .then(ui.renderPosts)
   .catch( (error) => {
     return console.log(error);
+  });
+
+User.findRecent()
+  .then(ui.renderUsers)
+  .catch( (error) => {
+    console.log(error);
   });
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,5 @@
+import xss from "xss-filters";
+
 let ui = {
   renderPosts(posts){
     let elements = posts.map((post) => {
@@ -6,17 +8,42 @@ let ui = {
     });
     let target = document.querySelector(".container");
     target.innerHTML = elements.join("");
+  },
+
+  renderUsers(users){
+    let elements = users.map( (user) => {
+      let {name, avatar } = user;
+      return userTemplate(name, avatar);
+    });
+
+    let target = document.querySelector(".sidebar-content");
+    target.innerHTML = elements.join("");
   }
 }
 
+function userTemplate(name, avater){
+  let safeName = xss.inHTMLData(name);
+  let safeAvatar = xss.inHTMLData(avater);
+
+  let template = `
+    <div class="active-avatar">
+      <img width="54" src="assets/images/${safeAvatar}">
+      <h5 class="post-author">${safeName}</h5>
+    </div>`;
+
+    return template
+}
+
 function articleTemplate(title, lastReply){
+  let safeTitle = xss.inHTMLData(title);
+  let safeLastReply = xss.inHTMLData(lastReply)
   let template = `
     <article class="post">
       <h2 class="post-title">
-        ${title}
+        ${safeTitle}
       </h2>
       <p class="post-meta">
-        ${lastReply}
+        ${safeLastReply}
       </p>
     </article>`;
 
